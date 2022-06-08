674d631e86f1e8b57b2bb36643bf1aa0fdc8a0cb
juliafan55 <juliafan55@gmail.com>
2022-06-08 11:09:01 -0400

    redux folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom"
 import Login from "./pages/login/login";
 import Profile from "./pages/profile/profile";
+import Home from "./pages/home/home";
 
 function App() {
   return (
     <div>
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/profile" element={<Profile />} />
       </Routes>
