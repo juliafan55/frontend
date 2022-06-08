dc310168d708604e6f249d45c850017d88468344
juliafan55 <juliafan55@gmail.com>
2022-06-08 12:31:33 -0400

    login page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form } from "formik";
+import { Link } from "react-router-dom";
+import "./home.css"
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="login">
+            <div className="login-wrapper">
+                <div className="login-wrap">
+                    <div className="login-1">
+                        <img src="../../icons/digital-hub.svg" alt="logo"></img>
+                        <span>Digital Hub collects your memories and help share it with the people in your life.</span>
+                    </div>
+                    <div className="login-2">
+                        <div className="login-2-wrap">
+                            <Formik>
+                                {(formik) => (
+                                    <Form>
+                                        <input type="text" />
+                                        <input type="text" />
+                                        <button type="submit" className="pink-btn">Log In</button>
+                                    </Form>
+                                    )
+                                }
+                            </Formik>
+                            <Link to="/forgot" className="forgot-password"> Forgot your password?</Link>
+                            <div className="sign-splitter"></div>
+                            <button className="pink-btn open-signup">Create Account</button>
+                        </div>
+                    </div>
+                </div>
+                <div className="register"></div>
+            </div>
+
+            
         </div>
     )
 }
