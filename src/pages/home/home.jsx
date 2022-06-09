b4279982ab020a66a23a20f5af99d0b4a6ea3bb4
juliafan55 <juliafan55@gmail.com>
2022-06-09 08:03:10 -0400

    revised file structure

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,19 +1,11 @@
-import React,{useRef, useState} from 'react'
-import Header from '../../components/header/Header'
-import useClickOutside from '../../helpers/clickOutside'
+import { useRef, useState } from "react";
+import Header from "../../components/header/Header";
 
-export default function Login() {
-    const [visible, setVisible] = useState(true)
-    const el = useRef(null)
 
-    useClickOutside(el, () => {
-        setVisible(false);
-    })
-    
-    return (
-        <div>
-            <Header />
-            {visible && <div className="card" ref={el}></div>}
-        </div>
-    )
+export default function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -2,11 +2,7 @@ import "./login.css"
 import LoginForm from "../../components/login/LoginForm"
 import RegisterForm from "../../components/login/RegisterForm"
 import { useState } from "react"
-import DotLoader from "react-spinners/DotLoader"
-import { useDispatch } from "react-redux";
-import axios from "axios"
-import Cookies from "js-cookie"
-import { useNavigate } from "react-router-dom";
+
 
 export default function Login() {
     const [visible, setVisible] = useState(false)
