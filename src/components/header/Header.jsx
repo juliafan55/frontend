0e3e8c6e6a4812d4577cdddec416838c15eb7cc8
juliafan55 <juliafan55@gmail.com>
2022-06-08 21:25:15 -0400

    home page header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import "./header.css"
 import {Link } from "react-router-dom"
-import { Logo, Search } from '../../svg'
+import { HomeActive, Search, Friends, Messenger, Menu, Notifications, ArrowDown } from '../../svg'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
+    const { user } = useSelector((user) => ({ ...user }));
+    const color = "#001858";
     
     return <header>
         <div className="header-left">
@@ -17,7 +20,30 @@ export default function Header() {
                 <input type="text" placeholder="Search Digital Hub" className="hide-input"></input>
             </div>
         </div>
-        <div className="header-middle"></div>
-        <div className="header-right"></div>
+        <div className="header-middle">
+            <Link to="/" className="middle-icon hover-pink">
+                <HomeActive color={color} />
+            </Link>
+            <Link to="/" className="middle-icon hover-pink">
+                <Friends color={color} />
+                <div className="middle-notification">4+</div>
+            </Link>
+            
+        </div>
+        <div className="header-right">
+            <Link to="/profile" className="profile-link hover-pink">
+                <img src={user?.picture} alt="profile" />
+                <span>{user?.first_name}</span>
+            </Link>
+            <div className="circle-icon hover-pink">
+                <Menu />
+            </div>
+            <div className="circle-icon hover-pink">
+                <Messenger />
+            </div>
+            <div className="circle-icon hover-pink">
+                <ArrowDown />
+            </div>
+        </div>
     </header>
 }
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useRef, useState} from 'react'
 import Header from '../../components/header/Header'
+import useClickOutside from '../../helpers/clickOutside'
 
 export default function Login() {
+    const [visible, setVisible] = useState(true)
+    const el = useRef(null)
+
+    useClickOutside(el, () => {
+        setVisible(false);
+    })
+    
     return (
         <div>
-           <Header />
+            <Header />
+            {visible && <div className="card" ref={el}></div>}
         </div>
     )
 }
